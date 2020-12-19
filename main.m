53fc809c222abd7dca1c7ed72a13a44f0f1058c7
ChengChengYang1997 <kb240608@gmail.com>
2020-12-19 15:11:35 +0800

    initialize the states and the errors

--- main.m
+++ main.m
@@ -4,18 +4,28 @@ close all;
 % simulation time
 dt = 0.01;
 sim_t = 10;
-t = 0:dt:sim_t;
-t = t';
 
 % initialize parameters
 multirotor = multirotor_dynamics;
+multirotor.dt = dt;
+multirotor.sim_t = sim_t;
+multirotor.t = 0:dt:sim_t;
 multirotor.m = 4.34;
 multirotor.J = [0.0820, 0, 0;
                 0, 0.0845, 0;
                 0, 0, 0.1377];
 multirotor.d = 0.315;
 multirotor.c_tau = 8.004e-4;
+multirotor.x = zeros(3, length(multirotor.t));
+multirotor.v = zeros(3, length(multirotor.t));
+multirotor.R = zeros(9, length(multirotor.t));
+multirotor.W = zeros(3, length(multirotor.t));
+multirotor.ex = zeros(3, length(multirotor.t));
+multirotor.ev = zeros(3, length(multirotor.t));
+multirotor.eR = zeros(3, length(multirotor.t));
+multirotor.eW = zeros(3, length(multirotor.t));
+multirotor.R(1:9) = [1; 0; 0; 0; 1; 0 ;0 ; 0; 1];
 
 % initialize trajectory
-tra = zeros(9, length(t));
+tra = zeros(9, length(multirotor.t));
 traj = trajectory;
--- multirotor_dynamics.m
+++ multirotor_dynamics.m
@@ -1,13 +1,30 @@
 classdef multirotor_dynamics
     properties
+        % simulation time
+        dt = 0.01;
+        sim_t = 10;
+        t
+        iter
+        % parameters
         m
         J
         d
         c_tau
         g = 9.81;
+        % unit vector
         e1 = [1; 0; 0];
         e2 = [0; 1; 0];
         e3 = [0; 0; 1];
+        % states
+        x
+        v
+        R
+        W
+        % errors
+        ex
+        ev
+        eR
+        eW
     end
     methods
         function Dynamics = dynamics(obj)
