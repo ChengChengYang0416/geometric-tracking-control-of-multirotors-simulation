db507ec46e93e701b1c668cebe191b1227ecab28
ChengChengYang1997 <kb240608@gmail.com>
2020-12-19 14:51:10 +0800

    set simulaiton time and initialize the trajectory

--- main.m
+++ main.m
@@ -1,7 +1,13 @@
 % A simulation for geometric tracking control of multirotors
 close all;
 
-% initialize
+% simulation time
+dt = 0.01;
+sim_t = 10;
+t = 0:dt:sim_t;
+t = t';
+
+% initialize parameters
 multirotor = multirotor_dynamics;
 multirotor.m = 4.34;
 multirotor.J = [0.0820, 0, 0;
@@ -9,3 +15,7 @@ multirotor.J = [0.0820, 0, 0;
                 0, 0, 0.1377];
 multirotor.d = 0.315;
 multirotor.c_tau = 8.004e-4;
+
+% initialize trajectory
+tra = zeros(9, length(t));
+traj = trajectory;
