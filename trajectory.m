516b2af00322ce091fdc9d2d1267bdcef5153c12
ChengChengYang1997 <kb240608@gmail.com>
2020-12-19 15:47:17 +0800

    add desired acceleration

--- trajectory.m
+++ trajectory.m
@@ -2,7 +2,7 @@ classdef trajectory
    methods
        function out = traj_generate(~, t)
            % xd, vd, b1d
-           out = zeros(9, 1);
+           out = zeros(12, 1);
            % xd
            out(1) = 0.4*t;
            out(2) = 0.4*sin(pi*t);
@@ -11,10 +11,14 @@ classdef trajectory
            out(4) = 0.4;
            out(5) = 0.4*pi*cos(pi*t);
            out(6) = -0.6*pi*sin(pi*t);
+           % ad
+           out(7) = 0;
+           out(8) = -0.4*pi*pi*sin(pi*t);
+           out(9) = -0.6*pi*pi*cos(pi*t);
            % b1d
-           out(7) = 1;
-           out(8) = 0;
-           out(9) = 0;
+           out(10) = 1;
+           out(11) = 0;
+           out(12) = 0;
        end
    end
 end
