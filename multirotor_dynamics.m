93b334161b7b6c920e8fd06d0ab74412cb49c3a4
ChengChengYang1997 <kb240608@gmail.com>
2020-12-19 13:55:41 +0800

    create multirotor_dynamics

---
+++ multirotor_dynamics.m
@@ -0,0 +1,17 @@
+classdef multirotor_dynamics
+    properties
+        m
+        J
+        d
+        c_tau
+        g = 9.81;
+        e1 = [1; 0; 0];
+        e2 = [0; 1; 0];
+        e3 = [0; 0; 1];
+    end
+    methods
+        function Dynamics = dynamics(obj)
+            Dynamics = obj.mass;
+        end
+    end
+end
