24c2f467bf5fdf4f3d4a8463a080a706b3845a7c
ChengChengYang1997 <kb240608@gmail.com>
2020-12-19 16:01:56 +0800

    calculate f and Rd

--- main.m
+++ main.m
@@ -27,5 +27,56 @@ multirotor.eW = zeros(3, length(multirotor.t));
 multirotor.R(1:9) = [1; 0; 0; 0; 1; 0 ;0 ; 0; 1];
 
 % initialize trajectory
-tra = zeros(9, length(multirotor.t));
+tra = zeros(12, length(multirotor.t));
 traj = trajectory;
+
+for i = 1:length(multirotor.t)
+    t_now = multirotor.t(i);
+    
+    % desired trajectory
+    tra(:, i) = traj.traj_generate(t_now);
+    xd = tra(1:3, i);
+    vd = tra(4:6, i);
+    ad = tra(7:9, i);
+    b1d = tra(10:12, i);
+    
+    % now states
+    x = multirotor.x(:, i);
+    v = multirotor.v(:, i);
+    R = reshape(multirotor.R(:, i), 3, 3);
+    W = multirotor.W(:, i);
+    e3 = multirotor.e3;
+    
+    % errors
+    ex = x - xd;
+    ev = v - vd;
+    
+    % control gains
+    kx = diag([16*multirotor.m; 16*multirotor.m; 16*multirotor.m]);
+    kv = diag([5.6*multirotor.m; 5.6*multirotor.m; 5.6*multirotor.m]);
+    kR = 8.81;
+    kW = 2.54;
+    
+    % calculate f and Rd
+    A = (-kx*ex - kv*ev + multirotor.m*ad + multirotor.m*multirotor.g*multirotor.e3);
+    b3 = R*e3;
+    f = vec_dot(A,b3);
+    norm_A = norm(A);
+    b3d = A/norm_A;
+    b2d = vec_cross(b3d, b1d);
+    norm_b2d = norm(b2d);
+    b2d = b2d/norm_b2d;
+    b1d = vec_cross(b2d, b3d);
+    Rd = [b1d b2d b3d]; 
+    
+    multirotor.ex(:, i) = ex;
+    multirotor.ev(:, i) = ev;
+end
+
+figure(1)
+subplot(3, 1, 1)
+plot(multirotor.t, tra(1, :))
+subplot(3, 1, 2)
+plot(multirotor.t, tra(2, :))
+subplot(3, 1, 3)
+plot(multirotor.t, tra(3, :))
