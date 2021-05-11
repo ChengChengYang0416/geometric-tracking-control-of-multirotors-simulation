ba7b90e5f584a90aa0141ed537d19ba0d0e38d21
Cheng-Cheng Yang <chengchengyang0416@gmail.com>
2021-05-11 19:54:25 +0800

    change default trajectory

--- trajectory.m
+++ trajectory.m
@@ -4,17 +4,17 @@ classdef trajectory
            % xd, vd, b1d
            out = zeros(12, 1);
            % xd
-           out(1) = 0.4*t;
-           out(2) = 0.4*sin(pi*t);
-           out(3) = 0.6*cos(pi*t);
+           out(1) = sin(1*t);
+           out(2) = sin(1*t);
+           out(3) = 1;
            % vd
-           out(4) = 0.4;
-           out(5) = 0.4*pi*cos(pi*t);
-           out(6) = -0.6*pi*sin(pi*t);
+           out(4) = 1*cos(1*t);
+           out(5) = 1*cos(1*t);
+           out(6) = 0;
            % ad
-           out(7) = 0;
-           out(8) = -0.4*pi*pi*sin(pi*t);
-           out(9) = -0.6*pi*pi*cos(pi*t);
+           out(7) = -1*1*sin(1*t);
+           out(8) = -1*1*sin(1*t);
+           out(9) = 0;
            % b1d
            out(10) = 1;
            out(11) = 0;
