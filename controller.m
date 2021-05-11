2faf5729db4c096aae4785485e949108149a06f9
Cheng-Cheng Yang <chengchengyang0416@gmail.com>
2021-05-11 18:57:36 +0800

    calculate control input in ned coordinate

--- controller.m
+++ controller.m
@@ -1,40 +1,47 @@
 classdef controller
    methods
-       function [control, error] = geometric_tracking_ctrl(obj, iter, multirotor, Xd, b1d)
+       function [control, error] = geometric_tracking_ctrl(obj, iter, multirotor, Xd_enu, b1d)
            % f, M
            control = zeros(16, 1);
            
            % xd, vd, ad, b1d, Wd
-           xd = Xd(1:3, 1);
-           vd = Xd(4:6, 1);
-           ad = Xd(7:9, 1);
+           xd_enu = Xd_enu(1:3, 1);
+           vd_enu = Xd_enu(4:6, 1);
+           ad_enu = Xd_enu(7:9, 1);
            Wd = [0; 0; 0];
            
            % now states
-           x = multirotor.x(:, iter-1);
-           v = multirotor.v(:, iter-1);
+           x_enu = multirotor.x(:, iter-1);
+           v_enu = multirotor.v(:, iter-1);
            R = reshape(multirotor.R(:, iter-1), 3, 3);
            W = multirotor.W(:, iter-1);
            e3 = multirotor.e3;
            
            % control gains
-           kx = diag([16*multirotor.m; 16*multirotor.m; 16*multirotor.m]);
-           kv = diag([5.6*multirotor.m; 5.6*multirotor.m; 5.6*multirotor.m]);
-           kR = 150;
+           kx = diag([9.0*multirotor.m; 9.0*multirotor.m; 9.0*multirotor.m]);
+           kv = diag([7.0*multirotor.m; 7.0*multirotor.m; 7.0*multirotor.m]);
+           kR = 8.81;
            kW = 2.54;
            
+           % convert position and velocity from enu to ned
+           x_ned = vec_enu_to_ned(x_enu);
+           v_ned = vec_enu_to_ned(v_enu);
+           xd_ned = vec_enu_to_ned(xd_enu);
+           vd_ned = vec_enu_to_ned(vd_enu);
+           ad_ned = vec_enu_to_ned(ad_enu);
+           
            % error
-           ex = x - xd;
-           ev = v - vd;
+           ex_ned = x_ned - xd_ned;
+           ev_ned = v_ned - vd_ned;
            
            % f
-           A = (-kx*ex - kv*ev + multirotor.m*ad + multirotor.m*multirotor.g*e3);
+           A = (-kx*ex_ned - kv*ev_ned + multirotor.m*ad_ned - multirotor.m*multirotor.g*e3);
            b3 = R*e3;
-           f = vec_dot(A, b3);
+           f = -vec_dot(A, b3);
            
            % Rd
            norm_A = norm(A);
-           b3d = A/norm_A;
+           b3d = -A/norm_A;
            b2d = vec_cross(b3d, b1d);
            norm_b2d = norm(b2d);
            b2d = b2d/norm_b2d;
@@ -55,8 +62,8 @@ classdef controller
            control(4) = M(3);
            
            % ex, ev, eR, eW
-           error(1:3) = ex;
-           error(4:6) = ev;
+           error(1:3) = ex_ned;
+           error(4:6) = ev_ned;
            error(7:9) = eR;
            error(10:12) = eW;
        end
